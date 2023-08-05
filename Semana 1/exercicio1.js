2f427450f85789c34deb27ff5df072e95deba1c6
wgeovanni <willyangeo@gmail.com>
2023-08-05 20:40:35 -0300

    feat: Exercicio 1 - Clean Code Class PrimeNumber

--- Semana 1/exercicio1.js
+++ Semana 1/exercicio1.js
@@ -1,22 +1,51 @@
-function primeNumber(number) {
-    let increment = 2;
-    let isPrime = false;
+// function primeNumber(number) {
+//     let increment = 2;
+//     let isPrime = false;
 
-    if (number === 2) isPrime = true;
+//     if (number === 2) isPrime = true;
 
-    while (increment < number) {
-        if (number % increment === 0) {
-            isPrime = false;
-            break;
-        } else {
-            isPrime = true;
+//     while (increment < number) {
+//         if (number % increment === 0) {
+//             isPrime = false;
+//             break;
+//         } else {
+//             isPrime = true;
+//         }
+//         increment++
+//     }
+
+//     return isPrime;
+// }
+
+// // Teste a função
+// console.log(primeNumber(7)); // Saída esperada: true
+// console.log(primeNumber(12)); // Saída esperada: false
+
+class VerifyPrime {
+
+    testPrime(number) {
+
+        let increment = 2;
+        let isPrime = false;
+
+        if (number === 2) isPrime = true;
+
+        while (increment < number) {
+            if (number % increment === 0) {
+                isPrime = false;
+                break;
+            } else {
+                isPrime = true;
+            }
+            increment++
         }
-        increment++
-    }
 
-    return isPrime;
+        return isPrime;
+    }
 }
 
+const teste = new VerifyPrime();
+
 // Teste a função
-console.log(primeNumber(7)); // Saída esperada: true
-console.log(primeNumber(12)); // Saída esperada: false
+console.log(teste.testPrime(7)); // Saída esperada: true
+console.log(teste.testPrime(12)); // Saída esperada: false
